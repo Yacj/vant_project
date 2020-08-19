60043b84232eac204231ba6bc5f6a6c013976d1d
yacj <1356888297@qq.com>
2020-08-19 10:18:51 +0800

    配置vue.config

--- Vue.config.js
+++ Vue.config.js
@@ -1,20 +1,40 @@
-const PtoViewPort = require('postcss-px-to-viewport')
+const PtoViewPort = require("postcss-px-to-viewport");
+const TerserPlugin = require("terser-webpack-plugin");
+
 module.exports = {
-    devServer: {
-        host: "192.168.0.100",
-        port: '8888',
-        open: true, //是否自动弹出浏览器页面
+  configureWebpack: (config) => {
+    // 返回一个将会被合并的对象
+    return {
+      optimization: {
+        minimizer: [
+          new TerserPlugin({
+            sourceMap: false,
+            terserOptions: {
+              compress: {
+                drop_console: true,
+              },
+            },
+          }),
+        ],
+      },
+    };
+  },
+  productionSourceMap: false,
+  devServer: {
+    host: "192.168.0.100",
+    port: "8888",
+    open: true, //是否自动弹出浏览器页面
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          PtoViewPort({
+            viewportWidth: 375,
+            selectorBlackList: ["vant"],
+          }),
+        ],
+      },
     },
-    css:{
-        loaderOptions:{
-            postcss: {
-                plugins: [
-                    PtoViewPort({
-                        viewportWidth: 375,
-                        selectorBlackList: ['vant']
-                    })
-                ]
-            }
-        }
-    }
-}
+  },
+};
