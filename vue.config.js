82036b96214e0838347a2ae086f0e3a53d869c7d
Yacj <1356888297@qq.com>
2020-10-08 19:20:01 +0800

    2020-10-08 更新npm源

--- vue.config.js
+++ vue.config.js
@@ -32,7 +32,7 @@ module.exports = {
       sass: {
         // @/ 是 src/ 的别名
         // 所以这里假设你有 `src/variables.scss` 这个文件
-        prependData: `@import "src/assets/css/variables.scss";`
+        additionalData: `@import "src/assets/css/variables.scss";`
       },
       postcss: {
         plugins: [
