418694cb4b1c64466ed3b2cb69cd739e8a5cf6d9
Yacj <1356888297@qq.com>
2020-12-21 11:38:38 +0800

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,3 +6,14 @@
 export function isUndef(v) {
     return v === undefined || v === null;
 }
+
+/**
+ * 判断传入值的类型
+ * @param obj
+ * getType() // Undefined
+ * getType(null) // Null
+ * getType(NaN) // Number
+ */
+export function isCheckType(obj){
+    return Object.prototype.toString.call(obj).slice(8, -1)
+}
